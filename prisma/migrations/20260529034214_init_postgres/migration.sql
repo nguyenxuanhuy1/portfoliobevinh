f54dfaa8330e4ac9f8f14206b57c958c55f248c3
Vinh Nguyen Duc <nguyenducvinh77102006@gmail.com>
2026-06-16 08:20:00 +0700

    update

--- prisma/migrations/20260529034214_init_postgres/migration.sql
+++ prisma/migrations/20260529034214_init_postgres/migration.sql
@@ -91,6 +91,26 @@ CREATE TABLE "learn_topics" (
     CONSTRAINT "learn_topics_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "documents" (
+    "id" TEXT NOT NULL,
+    "subject" VARCHAR(255) NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "description" TEXT NOT NULL,
+    "level" INTEGER NOT NULL DEFAULT 0,
+    "downloads" INTEGER NOT NULL DEFAULT 0,
+    "views" INTEGER NOT NULL DEFAULT 0,
+    "file_type" VARCHAR(50) NOT NULL,
+    "file_size" VARCHAR(50) NOT NULL,
+    "tags" TEXT[],
+    "md_download_url" VARCHAR(500) NOT NULL,
+    "download_url" VARCHAR(500) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "documents_pkey" PRIMARY KEY ("id")
+);
+
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
